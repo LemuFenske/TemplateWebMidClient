1893885d84a528f54da05ec58a7ee89c35bd2f25
Lemuel Fenske <lemueljosias.03@gmail.com>
2023-07-27 19:29:13 -0300

    commit server

---
+++ Proyectos/PortfolioLemuelFenske/server/Controllers/Mailer.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
+
+
+module.exports = transporter;

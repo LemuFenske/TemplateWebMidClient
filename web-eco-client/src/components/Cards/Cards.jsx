5bf97886d57465395f0a03a2e9f86f7d70c6bb89
Lemuel Fenske <lemueljosias.03@gmail.com>
2023-08-02 20:47:16 -0300

    lemu2

--- web-eco-client/src/components/Cards/Cards.jsx
+++ web-eco-client/src/components/Cards/Cards.jsx
@@ -8,6 +8,7 @@ export default function Cards() {
         <div className="flex flex-wrap z-50 justify-around ">
             {plans.map(plan => {
                 return <Card 
+                key={plan.name}
                 name={plan.name}
                 price={plan.price}
                 duration={plan.duration}
